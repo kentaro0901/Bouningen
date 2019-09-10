a56a80fcbf10cd5b6a81bd9d5f56bf5e1ccdc4ea
Kentaro Okino <ken0901sm@gmail.com>
2019-09-10 10:14:53 +0900

    ChaseCameraの調整

--- Assets/Scripts/ChaseCamera.cs
+++ Assets/Scripts/ChaseCamera.cs
@@ -2,31 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChaseCamera : MonoBehaviour{
+public class ChaseCamera : MonoBehaviour {
 
     public Transform chaseTf;
-    Vector3[] chaseTfPrePos;
     Transform chaseCameraTf;
     Vector3 chaseCameraInitPos;
+    Camera _camera;
+    [SerializeField] float chaseRange = 1.0f;
 
     void Start(){
-        chaseTfPrePos = new Vector3[3];
         chaseCameraTf = this.gameObject.transform;
         chaseCameraInitPos = chaseCameraTf.position;
+        _camera = GetComponentInChildren<Camera>();
+        chaseRange = -_camera.ScreenToWorldPoint(new Vector3(Screen.width / 4, Screen.height / 2, -10)).x;
     }
 
     void Update(){
-        if(chaseTf != null) {
-            if(chaseTf.position.y < 4) {
-                chaseCameraTf.position = chaseCameraInitPos + new Vector3(chaseTfPrePos[2].x, 0, 0);
-            }
-            else {
-                chaseCameraTf.position = chaseCameraInitPos + new Vector3(chaseTfPrePos[2].x, chaseTf.position.y - 4, 0);
-            }
-
-            chaseTfPrePos[2] = chaseTfPrePos[1];
-            chaseTfPrePos[1] = chaseTfPrePos[0];
-            chaseTfPrePos[0] = chaseTf.position;
+        if(chaseCameraTf.position.x + chaseRange < chaseTf.position.x) { //右
+            chaseCameraTf.position = new Vector3(chaseTf.position.x - chaseRange, chaseCameraInitPos.y, chaseCameraInitPos.z);
         }
+        if (chaseTf.position.x < chaseCameraTf.position.x -chaseRange) { //左
+            chaseCameraTf.position = new Vector3(chaseTf.position.x + chaseRange, chaseCameraInitPos.y, chaseCameraInitPos.z);
+        }
+
     }
 }
