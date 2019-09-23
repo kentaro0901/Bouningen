fc989f58434547e0a38fb4a99402d743eb5e4027
Kentaro Okino <ken0901sm@gmail.com>
2019-09-23 18:25:16 +0900

    Select画面の追加

--- Assets/Scripts/Mgr/SelectMgr.cs
+++ Assets/Scripts/Mgr/SelectMgr.cs
@@ -24,29 +24,65 @@ public class SelectMgr : MonoBehaviour {
         }
     }
 
+
+    [SerializeField] RectTransform Frame1PRTf;
+    [SerializeField] RectTransform Frame2PRTf;
+    int count1 = 1;
+    int count2 = 1;
+    bool isReleseAxis1 = true;
+    bool isReleseAxis2 = true;
+    bool isMovable1 = true;
+    bool isMovable2 = true;
+
     void Start() {
         Main.state = Main.State.Select;
         Main.CameraSetting();
+        Frame1PRTf.sizeDelta = new Vector2(Screen.width / 2, Frame1PRTf.sizeDelta.y);
+        Frame1PRTf.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width / 2, Frame1PRTf.sizeDelta.y);
+        Frame2PRTf.sizeDelta = new Vector2(Screen.width / 2, Frame2PRTf.sizeDelta.y);
+        Frame2PRTf.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width / 2, Frame2PRTf.sizeDelta.y);
+        Frame1PRTf.localPosition = new Vector3(Frame1PRTf.localPosition.x + Screen.width / 4, Frame1PRTf.localPosition.y);
+        Frame2PRTf.localPosition = new Vector3(Frame2PRTf.localPosition.x + Screen.width / 4, Frame2PRTf.localPosition.y);
+
     }
 
     void Update() {
-        if(Input.GetKeyDown(KeyCode.Space)) {//仮
-            //SceneManager.LoadScene("Battle");
+        //1P
+        if (Input.GetAxis("DPad_XAxis_1") > 0 && isReleseAxis1 && count1 < 2 && isMovable1) {
+            isReleseAxis1 = false;
+            count1++;
+            iTween.MoveBy(Frame1PRTf.gameObject, iTween.Hash("x",Screen.width / 4, "time", 0.3f, "oncomplete", "MoveEnd1", "oncompletetarget", gameObject));          
+        }
+        else if (Input.GetAxis("DPad_XAxis_1") < 0 && isReleseAxis1 && 0 < count1 && isMovable1) {
+            isReleseAxis1 = false;
+            count1--;
+            iTween.MoveBy(Frame1PRTf.gameObject, iTween.Hash("x", -Screen.width / 4, "time", 0.3f, "oncomplete", "MoveEnd1", "oncompletetarget", gameObject));
+        }
+
+        //2P
+        if (Input.GetAxis("DPad_XAxis_2") > 0 && isReleseAxis2 && count2 < 2 && isMovable2) {
+            isReleseAxis2 = false;
+            count2++;
+            iTween.MoveBy(Frame2PRTf.gameObject, iTween.Hash("x", Screen.width / 4, "time", 0.3f, "oncomplete", "MoveEnd2", "oncompletetarget", gameObject));
+        }
+        else if (Input.GetAxis("DPad_XAxis_2") < 0 && isReleseAxis2 && 0 < count2 && isMovable2) {
+            isReleseAxis2 = false;
+            count2--;
+            iTween.MoveBy(Frame2PRTf.gameObject, iTween.Hash("x", -Screen.width / 4, "time", 0.3f, "oncomplete", "MoveEnd2", "oncompletetarget", gameObject));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {//仮
+            isMovable1 = false;
+            isMovable2 = false;
             FadeManager.Instance.LoadScene("Battle", 0.5f);
             Main.state = Main.State.Battle;
         }
     }
 
-    public void ButtonSword1P() {
-        Main.Instance.chara1P = Main.Chara.Sword;
+    void MoveEnd1() {
+        isReleseAxis1 = true;
     }
-    public void ButtonFighter1P() {
-        Main.Instance.chara1P = Main.Chara.fIghter;
-    }
-    public void ButtonSword2P() {
-        Main.Instance.chara1P = Main.Chara.Sword;
-    }
-    public void ButtonFighter2P() {
-        Main.Instance.chara1P = Main.Chara.fIghter;
+    void MoveEnd2() {
+        isReleseAxis2 = true;
     }
 }
