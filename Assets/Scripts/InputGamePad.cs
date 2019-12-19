8aabed75dede3187025406b3e6c76fef7e6af8b8
pino0901 <ken0910bw2@gmail.com>
2019-12-19 22:10:57 +0900

    バグ解消

--- Assets/Scripts/InputGamePad.cs
+++ Assets/Scripts/InputGamePad.cs
@@ -6,7 +6,7 @@ public class InputGamePad : InputMethod {
 
     int num;
     private void Start() {
-        num = Main.Instance.isSwapController? (int)controller.playerNum+1 : (int)controller.playerNum;
+        num = Main.Instance.isSwapController? ((int)controller.playerNum == 1?2:1) : (int)controller.playerNum;
     }
 
     void Update() {
